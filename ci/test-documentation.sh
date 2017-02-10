41934a36b32a1db63671c1748145ec647cbe4aa5
Lars Schneider <larsxschneider@gmail.com>
2017-02-10 22:58:20 +0100

    ascii

--- ci/test-documentation.sh
+++ ci/test-documentation.sh
@@ -4,11 +4,21 @@
 #
 
 set -e
+set -x
 
 make check-builtins
 make check-docs
-make doc
 
+# Build docs with AsciiDoc
+make clean
+make --jobs=2 doc
+grep '<meta name="generator" content="AsciiDoc ' Documentation/git.html
 test -s Documentation/git.html
 test -s Documentation/git.xml
 test -s Documentation/git.1
+
+# Build docs with AsciiDoctor
+make clean
+make --jobs=2 doc USE_ASCIIDOCTOR=1
+grep '<meta name="generator" content="Asciidoctor ' Documentation/git.html
+test -s Documentation/git.html
