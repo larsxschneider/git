92861e1aec99e95b0552c1ed9e6014c94e24c52d
Lars Schneider <larsxschneider@gmail.com>
2017-09-10 20:37:19 +0200

    sdf
    Signed-off-by: Lars Schneider <larsxschneider@gmail.com>

--- ci/run-windows-build.sh
+++ ci/run-windows-build.sh
@@ -93,7 +93,7 @@ do
 	if test $(($(date +%s) - $START)) -ge 0 # 9000 sec = 2.5h
 	then
 		printf "\n\nBuild #${BUILD_ID} hit the timeout. Checking previous builds ..."
-		tac $PREVIOUS_BUILD_IDS | tail -n+1 | while read PREVIOUS
+		while read PREVIOUS
 		do
 			echo "AAAAAA xx$PREVIOUS xx"
 			STATUS=$(gfwci "action=status&buildId=$PREVIOUS")
@@ -106,7 +106,7 @@ do
 					break
 					;;
 			esac
-		done
+		done <<< $(tac $PREVIOUS_BUILD_IDS | tail -n+2)
 	fi
 	STATUS=$(gfwci "action=status&buildId=$BUILD_ID")
 
